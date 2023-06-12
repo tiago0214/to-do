f4108f8d82f2cd7c6b56cdd326a6eb422a0ef40e
Tiago Souza <tiago0214@gmail.com>
2023-06-12 13:59:37 -0300

    tas

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 function toggleMode() {
-  const html = document.documentElement
+  const html = document.documentElement;
   if (html.classList.contains("light")) {
-    html.classList.remove("light")
+    html.classList.remove("light");
   } else {
-    html.classList.add("light")
+    html.classList.add("light");
   }
 
-  const img = document.querySelector("#profile img")
+  const img = document.querySelector("#profile img");
   if (html.classList.contains("light")) {
-    img.setAttribute(
-      "src",
-      "./assets/avatar-light.png"
-    )
+    img.setAttribute("src", "./assets/avatar-light.png");
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar.png");
   }
 }
 
-document.getElementById('formacao').addEventListener("click",function arrumar(link) {
-  
-  console.log(link)
-  link.preventDefault()
-  const conteudo = document.getElementById('conteudo')
-                fetch(link.target.href)
-                    .then(resp => resp.text())
-                    .then(html => conteudo.innerHTML = html)
-}
-)
+document.getElementById("formacao").addEventListener("click", function arrumar(link) {
+  link.preventDefault();
+  const conteudo = document.getElementById("conteudo");
+  fetch(link.target.href)
+    .then((resp) => resp.text())
+    .then((html) => (conteudo.innerHTML = html));
+});
